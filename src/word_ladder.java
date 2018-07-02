a104134c7b2c09dcacdf595f263cca7f347f3d49
Lin Yixuan <2442548803@sjtu.edu.cn>
2018-07-02 20:31:56 +0800

    edition 1

--- src/word_ladder.java
+++ src/word_ladder.java
@@ -1,21 +1,19 @@
-    
 
-import java.io.File;  
+import java.io.File;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.BufferedReader;  
-import java.io.BufferedWriter;  
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;  
-    
-    
+import java.io.IOException;
+
 public class word_ladder {
-	public static int ladder(String start, String end, List<String> dict) {
-		if (start == null || end == null || start.length() == 0 || end.length() == 0 || start.length() != end.length() ) {
+	static int ladder(String start, String end, List<String> dict) {
+		if (start.length() != end.length()) {
 			System.out.println("No ladder.");
 			return 0;
 		}
@@ -29,73 +27,95 @@ public class word_ladder {
 		all.add(cases);
 		int n = all.size();
 		char[] endunit = end.toCharArray();
-		while(true) {
-			for(int i = 0; i < n; i++) {
+		while (true) {
+			for (int i = 0; i < n; i++) {
 				cases = all.get(i);
-				for(int j = 0; j < start.length(); j++) {
+				for (int j = 0; j < start.length(); j++) {
 					String temp = cases.getLast();
 					char[] wordunit = temp.toCharArray();
 					wordunit[j] = endunit[j];
 					temp = new String(wordunit);
-					if(temp.equals(end)) {
+					if (temp.equals(end)) {
 						cases.add(temp);
-						System.out.println("Found ladder:");
+						System.out.printf("A ladder from %s to %s:\n", start, end);
 						for (int a = 0; a < cases.size(); a++) {
-							System.out.println(cases.get(a));
+							System.out.print(cases.get(a) + " ");
 						}
+						System.out.println("");
 						return 0;
-					}
-					else if(dict.contains(temp)&&!cases.contains(temp)) {
+					} else if (dict.contains(temp) && !cases.contains(temp)) {
 						cases.add(temp);
 						all.add(cases);
-					}
-					else temp = cases.getLast();
+					} else
+						temp = cases.getLast();
 				}
-				for(int k = 0; k < n; k++) all.removeFirst();
+				for (int k = 0; k < n; k++)
+					all.removeFirst();
 				n = all.size();
-				if(n == 0) { 
+				if (n == 0) {
 					System.out.println("No ladder.");
 					return 0;
 				}
 			}
 		}
 	}
-	    public static List<String> read(File txt){
-	        StringBuilder result = new StringBuilder();
-	        List<String> dict = new ArrayList<String>();
-	        try{
-	            BufferedReader br = new BufferedReader(new FileReader(txt));
-	            String s = null;
-	            while((s = br.readLine())!=null){
-	            	dict.add(s);
-	                result.append(System.lineSeparator()+s);
-	            }
-	            br.close();
-	        }catch(Exception e){
-	            e.printStackTrace();
-	        }
-	        return dict;
-	    }
-	    
-	    public static String ReadTest(){   
-	        System.out.println("Please Enter a word:");   
-	        InputStreamReader is = new InputStreamReader(System.in);    
-	        BufferedReader br = new BufferedReader(is);
-	        String word = "";
-	        try{   
-	          word = br.readLine();
-	        }   
-	        catch(IOException e){   
-	          e.printStackTrace();   
-	        }
-	        return word;
-	      }  
-	    
-	    public static void main(String[] args){
-	        File direct = new File("smalldict1.txt");
-	        List<String> dict = read(direct);
-	        String start = ReadTest();
-	        String end = ReadTest();
-	        ladder(start,end,dict);
-	    }
+
+	static List<String> read(File txt) {
+		StringBuilder result = new StringBuilder();
+		List<String> dict = new ArrayList<String>();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(txt));
+			String s = null;
+			while ((s = br.readLine()) != null) {
+				dict.add(s);
+				result.append(System.lineSeparator() + s);
+			}
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return dict;
 	}
+
+	static String ReadTest() {
+		InputStreamReader is = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(is);
+		String word = "";
+		try {
+			word = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if (word == null || word.length() <= 0) {
+			System.out.println("Have a nice day!");
+			System.exit(0);
+		}
+		return word;
+	}
+
+	static File getDirect() {
+		String filename = ReadTest();
+		File direct = new File(filename);		
+		if(!direct.exists()) {
+			System.out.println("No such file.");
+			return null;
+		}
+		return direct;
+	}
+
+	public static void main(String[] args) {
+		File direct = null;
+		while (direct == null) {
+			System.out.print("Dictionary file name? (or Enter to quit): ");
+			direct = getDirect();
+		}
+		List<String> dict = read(direct);
+		while (true) {
+			System.out.print("Word #1 (or Enter to quit): ");
+			String start = ReadTest();
+			System.out.print("Word #2 (or Enter to quit): ");
+			String end = ReadTest();
+			ladder(start, end, dict);
+		}
+	}
+}
